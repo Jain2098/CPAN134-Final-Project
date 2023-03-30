f9b2cf1b6adad8a0cb941a34c449f788a9eb00bf
Himanshu <himanshu@gmail.com>
2023-03-30 12:22:55 -0400

    BASE commit

---
+++ contact.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name == "" || email == "" || message == "") {
+        alert("Please fill out all the mandatory fields.");
+        return false;
+    }
+    else {
+    alert("Thanks for contacting us!\n\nYour message has been sent successfully.\nWe will get back to you as soon as possible.");
+}
+}
